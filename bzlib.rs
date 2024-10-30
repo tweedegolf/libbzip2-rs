57effb039787f98697b62c08117ea012118df913
Folkert de Vries <folkert@folkertdev.nl>
2024-10-30 10:56:55 +0100

    `macro ADD_CHAR_TO_BLOCK`: reinstate this macro

--- bzlib.rs
+++ bzlib.rs
@@ -493,6 +493,43 @@ unsafe fn flush_RL(s: &mut EState) {
     }
     init_RL(s);
 }
+
+macro_rules! BZ_UPDATE_CRC {
+    ($crcVar:expr, $cha:expr) => {
+        let index = ($crcVar >> 24) ^ ($cha as core::ffi::c_uint);
+        $crcVar = ($crcVar << 8) ^ BZ2_CRC32TABLE[index as usize];
+    };
+}
+
+macro_rules! ADD_CHAR_TO_BLOCK {
+    ($zs:expr, $zchh0:expr) => {
+        let s = $zs;
+        let zchh: u32 = $zchh0 as u32;
+
+        if zchh != (*s).state_in_ch && (*s).state_in_len == 1 {
+            /*-- fast track the common case --*/
+
+            let ch: u8 = (*s).state_in_ch as u8;
+            BZ_UPDATE_CRC!((*s).blockCRC, ch);
+            (*s).inUse[(*s).state_in_ch as usize] = 1;
+            *((*s).block).offset((*s).nblock as isize) = ch;
+            (*s).nblock += 1;
+            (*s).nblock;
+            (*s).state_in_ch = zchh;
+        } else if zchh != (*s).state_in_ch || (*s).state_in_len == 255 {
+            /*-- general, uncommon cases --*/
+
+            if (*s).state_in_ch < 256 {
+                add_pair_to_block(s);
+            }
+            (*s).state_in_ch = zchh;
+            (*s).state_in_len = 1;
+        } else {
+            (*s).state_in_len += 1;
+        }
+    };
+}
+
 unsafe fn copy_input_until_stop(s: *mut EState) -> Bool {
     let mut progress_in: Bool = 0 as Bool;
     if (*s).mode == 2 as libc::c_int {
@@ -500,29 +537,11 @@ unsafe fn copy_input_until_stop(s: *mut EState) -> Bool {
             if (*s).nblock >= (*s).nblockMAX {
                 break;
             }
-            if (*(*s).strm).avail_in == 0 as libc::c_int as libc::c_uint {
+            if (*(*s).strm).avail_in == 0 {
                 break;
             }
-            progress_in = 1 as Bool;
-            let zchh: u32 = *((*(*s).strm).next_in as *mut u8) as u32;
-            if zchh != (*s).state_in_ch && (*s).state_in_len == 1 as libc::c_int {
-                let ch: u8 = (*s).state_in_ch as u8;
-                (*s).blockCRC = (*s).blockCRC << 8 as libc::c_int
-                    ^ BZ2_CRC32TABLE
-                        [((*s).blockCRC >> 24 as libc::c_int ^ ch as libc::c_uint) as usize];
-                (*s).inUse[(*s).state_in_ch as usize] = 1 as Bool;
-                *((*s).block).offset((*s).nblock as isize) = ch;
-                (*s).nblock += 1;
-                (*s).state_in_ch = zchh;
-            } else if zchh != (*s).state_in_ch || (*s).state_in_len == 255 as libc::c_int {
-                if (*s).state_in_ch < 256 as libc::c_int as libc::c_uint {
-                    add_pair_to_block(s);
-                }
-                (*s).state_in_ch = zchh;
-                (*s).state_in_len = 1 as libc::c_int;
-            } else {
-                (*s).state_in_len += 1;
-            }
+            progress_in = 1;
+            ADD_CHAR_TO_BLOCK!(s, *((*(*s).strm).next_in as *mut u8) as u32);
             (*(*s).strm).next_in = ((*(*s).strm).next_in).offset(1);
             (*(*s).strm).avail_in = ((*(*s).strm).avail_in).wrapping_sub(1);
             (*(*s).strm).total_in_lo32 = ((*(*s).strm).total_in_lo32).wrapping_add(1);
@@ -535,36 +554,18 @@ unsafe fn copy_input_until_stop(s: *mut EState) -> Bool {
             if (*s).nblock >= (*s).nblockMAX {
                 break;
             }
-            if (*(*s).strm).avail_in == 0 as libc::c_int as libc::c_uint {
+            if (*(*s).strm).avail_in == 0 {
                 break;
             }
-            if (*s).avail_in_expect == 0 as libc::c_int as libc::c_uint {
+            if (*s).avail_in_expect == 0 {
                 break;
             }
-            progress_in = 1 as Bool;
-            let zchh_0: u32 = *((*(*s).strm).next_in as *mut u8) as u32;
-            if zchh_0 != (*s).state_in_ch && (*s).state_in_len == 1 as libc::c_int {
-                let ch_0: u8 = (*s).state_in_ch as u8;
-                (*s).blockCRC = (*s).blockCRC << 8 as libc::c_int
-                    ^ BZ2_CRC32TABLE
-                        [((*s).blockCRC >> 24 as libc::c_int ^ ch_0 as libc::c_uint) as usize];
-                (*s).inUse[(*s).state_in_ch as usize] = 1 as Bool;
-                *((*s).block).offset((*s).nblock as isize) = ch_0;
-                (*s).nblock += 1;
-                (*s).state_in_ch = zchh_0;
-            } else if zchh_0 != (*s).state_in_ch || (*s).state_in_len == 255 as libc::c_int {
-                if (*s).state_in_ch < 256 as libc::c_int as libc::c_uint {
-                    add_pair_to_block(s);
-                }
-                (*s).state_in_ch = zchh_0;
-                (*s).state_in_len = 1 as libc::c_int;
-            } else {
-                (*s).state_in_len += 1;
-            }
+            progress_in = 1;
+            ADD_CHAR_TO_BLOCK!(s, *((*(*s).strm).next_in as *mut u8) as u32);
             (*(*s).strm).next_in = ((*(*s).strm).next_in).offset(1);
             (*(*s).strm).avail_in = ((*(*s).strm).avail_in).wrapping_sub(1);
             (*(*s).strm).total_in_lo32 = ((*(*s).strm).total_in_lo32).wrapping_add(1);
-            if (*(*s).strm).total_in_lo32 == 0 as libc::c_int as libc::c_uint {
+            if (*(*s).strm).total_in_lo32 == 0 {
                 (*(*s).strm).total_in_hi32 = ((*(*s).strm).total_in_hi32).wrapping_add(1);
             }
             (*s).avail_in_expect = ((*s).avail_in_expect).wrapping_sub(1);
