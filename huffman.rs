7f53f11be3233fa3577d7429b4939693c3cdff66
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-10-30 12:21:07 +0100

    Various cleanups to huffman.rs

--- huffman.rs
+++ huffman.rs
@@ -1,5 +1,58 @@
 use crate::bzlib::BZ2_bz__AssertH__fail;
 pub type Bool = libc::c_uchar;
+
+const BZ_MAX_ALPHA_SIZE: usize = 258;
+const BZ_MAX_CODE_LEN: usize = 23;
+
+#[inline]
+unsafe fn upheap(
+    heap: &mut [i32; BZ_MAX_ALPHA_SIZE + 2],
+    weight: &mut [i32; BZ_MAX_ALPHA_SIZE * 2],
+    z: i32,
+) {
+    let mut zz: i32;
+    let tmp: i32;
+    zz = z;
+    tmp = heap[zz as usize];
+    while weight[tmp as usize] < weight[heap[(zz >> 1 as libc::c_int) as usize] as usize] {
+        heap[zz as usize] = heap[(zz >> 1 as libc::c_int) as usize];
+        zz >>= 1 as libc::c_int;
+    }
+    heap[zz as usize] = tmp;
+}
+
+#[inline]
+fn downheap(
+    heap: &mut [i32; BZ_MAX_ALPHA_SIZE + 2],
+    weight: &mut [i32; BZ_MAX_ALPHA_SIZE * 2],
+    nHeap: i32,
+    z: i32,
+) {
+    let mut zz: i32;
+    let mut yy: i32;
+    let tmp: i32;
+    zz = z;
+    tmp = heap[zz as usize];
+    loop {
+        yy = zz << 1 as libc::c_int;
+        if yy > nHeap {
+            break;
+        }
+        if yy < nHeap
+            && weight[heap[(yy + 1 as libc::c_int) as usize] as usize]
+                < weight[heap[yy as usize] as usize]
+        {
+            yy += 1;
+        }
+        if weight[tmp as usize] < weight[heap[yy as usize] as usize] {
+            break;
+        }
+        heap[zz as usize] = heap[yy as usize];
+        zz = yy;
+    }
+    heap[zz as usize] = tmp;
+}
+
 pub unsafe fn BZ2_hbMakeCodeLengths(len: *mut u8, freq: *mut i32, alphaSize: i32, maxLen: i32) {
     let mut nNodes: i32;
     let mut nHeap: i32;
@@ -8,152 +61,86 @@ pub unsafe fn BZ2_hbMakeCodeLengths(len: *mut u8, freq: *mut i32, alphaSize: i32
     let mut i: i32;
     let mut j: i32;
     let mut k: i32;
-    let mut tooLong: Bool;
-    let mut heap: [i32; 260] = [0; 260];
-    let mut weight: [i32; 516] = [0; 516];
-    let mut parent: [i32; 516] = [0; 516];
-    i = 0 as libc::c_int;
+    let mut tooLong: bool;
+    let mut heap = [0i32; BZ_MAX_ALPHA_SIZE + 2];
+    let mut weight = [0i32; BZ_MAX_ALPHA_SIZE * 2];
+    let mut parent = [0i32; BZ_MAX_ALPHA_SIZE * 2];
+    i = 0;
     while i < alphaSize {
-        weight[(i + 1 as libc::c_int) as usize] = (if *freq.offset(i as isize) == 0 as libc::c_int {
-            1 as libc::c_int
+        weight[(i + 1) as usize] = (if *freq.offset(i as isize) == 0 as libc::c_int {
+            1
         } else {
             *freq.offset(i as isize)
-        }) << 8 as libc::c_int;
+        }) << 8;
         i += 1;
     }
     loop {
         nNodes = alphaSize;
-        nHeap = 0 as libc::c_int;
-        heap[0 as libc::c_int as usize] = 0 as libc::c_int;
-        weight[0 as libc::c_int as usize] = 0 as libc::c_int;
-        parent[0 as libc::c_int as usize] = -2 as libc::c_int;
-        i = 1 as libc::c_int;
+        nHeap = 0;
+        heap[0] = 0;
+        weight[0] = 0;
+        parent[0] = -2;
+        i = 1;
         while i <= alphaSize {
-            parent[i as usize] = -1 as libc::c_int;
+            parent[i as usize] = -1;
             nHeap += 1;
             heap[nHeap as usize] = i;
-            let mut zz: i32;
-            let tmp: i32;
-            zz = nHeap;
-            tmp = heap[zz as usize];
-            while weight[tmp as usize] < weight[heap[(zz >> 1 as libc::c_int) as usize] as usize] {
-                heap[zz as usize] = heap[(zz >> 1 as libc::c_int) as usize];
-                zz >>= 1 as libc::c_int;
-            }
-            heap[zz as usize] = tmp;
+            upheap(&mut heap, &mut weight, nHeap);
             i += 1;
         }
-        if nHeap >= 258 as libc::c_int + 2 as libc::c_int {
-            BZ2_bz__AssertH__fail(2001 as libc::c_int);
+        if nHeap >= BZ_MAX_ALPHA_SIZE as libc::c_int + 2 {
+            BZ2_bz__AssertH__fail(2001);
         }
-        while nHeap > 1 as libc::c_int {
-            n1 = heap[1 as libc::c_int as usize];
-            heap[1 as libc::c_int as usize] = heap[nHeap as usize];
+        while nHeap > 1 {
+            n1 = heap[1];
+            heap[1] = heap[nHeap as usize];
             nHeap -= 1;
-            let mut zz_0: i32;
-            let mut yy: i32;
-            let tmp_0: i32;
-            zz_0 = 1 as libc::c_int;
-            tmp_0 = heap[zz_0 as usize];
-            loop {
-                yy = zz_0 << 1 as libc::c_int;
-                if yy > nHeap {
-                    break;
-                }
-                if yy < nHeap
-                    && weight[heap[(yy + 1 as libc::c_int) as usize] as usize]
-                        < weight[heap[yy as usize] as usize]
-                {
-                    yy += 1;
-                }
-                if weight[tmp_0 as usize] < weight[heap[yy as usize] as usize] {
-                    break;
-                }
-                heap[zz_0 as usize] = heap[yy as usize];
-                zz_0 = yy;
-            }
-            heap[zz_0 as usize] = tmp_0;
-            n2 = heap[1 as libc::c_int as usize];
-            heap[1 as libc::c_int as usize] = heap[nHeap as usize];
+            downheap(&mut heap, &mut weight, nHeap, 1);
+            n2 = heap[1];
+            heap[1] = heap[nHeap as usize];
             nHeap -= 1;
-            let mut zz_1: i32;
-            let mut yy_0: i32;
-            let tmp_1: i32;
-            zz_1 = 1 as libc::c_int;
-            tmp_1 = heap[zz_1 as usize];
-            loop {
-                yy_0 = zz_1 << 1 as libc::c_int;
-                if yy_0 > nHeap {
-                    break;
-                }
-                if yy_0 < nHeap
-                    && weight[heap[(yy_0 + 1 as libc::c_int) as usize] as usize]
-                        < weight[heap[yy_0 as usize] as usize]
-                {
-                    yy_0 += 1;
-                }
-                if weight[tmp_1 as usize] < weight[heap[yy_0 as usize] as usize] {
-                    break;
-                }
-                heap[zz_1 as usize] = heap[yy_0 as usize];
-                zz_1 = yy_0;
-            }
-            heap[zz_1 as usize] = tmp_1;
+            downheap(&mut heap, &mut weight, nHeap, 1);
             nNodes += 1;
             parent[n2 as usize] = nNodes;
             parent[n1 as usize] = parent[n2 as usize];
-            weight[nNodes as usize] = ((weight[n1 as usize] as libc::c_uint
-                & 0xffffff00 as libc::c_uint)
-                .wrapping_add(weight[n2 as usize] as libc::c_uint & 0xffffff00 as libc::c_uint)
-                | (1 as libc::c_int
-                    + (if weight[n1 as usize] & 0xff as libc::c_int
-                        > weight[n2 as usize] & 0xff as libc::c_int
-                    {
-                        weight[n1 as usize] & 0xff as libc::c_int
-                    } else {
-                        weight[n2 as usize] & 0xff as libc::c_int
-                    })) as libc::c_uint) as i32;
-            parent[nNodes as usize] = -1 as libc::c_int;
+            weight[nNodes as usize] = (weight[n1 as usize] & 0xffffff00u32 as i32)
+                .wrapping_add(weight[n2 as usize] & 0xffffff00u32 as i32)
+                | (1 + (if weight[n1 as usize] & 0xff > weight[n2 as usize] & 0xff {
+                    weight[n1 as usize] & 0xff
+                } else {
+                    weight[n2 as usize] & 0xff
+                }));
+            parent[nNodes as usize] = -1;
             nHeap += 1;
             heap[nHeap as usize] = nNodes;
-            let mut zz_2: i32;
-            let tmp_2: i32;
-            zz_2 = nHeap;
-            tmp_2 = heap[zz_2 as usize];
-            while weight[tmp_2 as usize]
-                < weight[heap[(zz_2 >> 1 as libc::c_int) as usize] as usize]
-            {
-                heap[zz_2 as usize] = heap[(zz_2 >> 1 as libc::c_int) as usize];
-                zz_2 >>= 1 as libc::c_int;
-            }
-            heap[zz_2 as usize] = tmp_2;
+            upheap(&mut heap, &mut weight, nHeap);
         }
-        if nNodes >= 258 as libc::c_int * 2 as libc::c_int {
-            BZ2_bz__AssertH__fail(2002 as libc::c_int);
+        if nNodes >= BZ_MAX_ALPHA_SIZE as libc::c_int * 2 {
+            BZ2_bz__AssertH__fail(2002);
         }
-        tooLong = 0 as Bool;
-        i = 1 as libc::c_int;
+        tooLong = false;
+        i = 1;
         while i <= alphaSize {
-            j = 0 as libc::c_int;
+            j = 0;
             k = i;
-            while parent[k as usize] >= 0 as libc::c_int {
+            while parent[k as usize] >= 0 {
                 k = parent[k as usize];
                 j += 1;
             }
-            *len.offset((i - 1 as libc::c_int) as isize) = j as u8;
+            *len.offset((i - 1) as isize) = j as u8;
             if j > maxLen {
-                tooLong = 1 as Bool;
+                tooLong = true;
             }
             i += 1;
         }
-        if tooLong == 0 {
+        if !tooLong {
             break;
         }
-        i = 1 as libc::c_int;
+        i = 1;
         while i <= alphaSize {
-            j = weight[i as usize] >> 8 as libc::c_int;
-            j = 1 as libc::c_int + j / 2 as libc::c_int;
-            weight[i as usize] = j << 8 as libc::c_int;
+            j = weight[i as usize] >> 8;
+            j = 1 + j / 2;
+            weight[i as usize] = j << 8;
             i += 1;
         }
     }
@@ -168,10 +155,10 @@ pub unsafe fn BZ2_hbAssignCodes(
     let mut n: i32;
     let mut vec: i32;
     let mut i: i32;
-    vec = 0 as libc::c_int;
+    vec = 0;
     n = minLen;
     while n <= maxLen {
-        i = 0 as libc::c_int;
+        i = 0;
         while i < alphaSize {
             if *length.offset(i as isize) as libc::c_int == n {
                 *code.offset(i as isize) = vec;
@@ -179,7 +166,7 @@ pub unsafe fn BZ2_hbAssignCodes(
             }
             i += 1;
         }
-        vec <<= 1 as libc::c_int;
+        vec <<= 1;
         n += 1;
     }
 }
@@ -196,10 +183,10 @@ pub unsafe fn BZ2_hbCreateDecodeTables(
     let mut i: i32;
     let mut j: i32;
     let mut vec: i32;
-    pp = 0 as libc::c_int;
+    pp = 0;
     i = minLen;
     while i <= maxLen {
-        j = 0 as libc::c_int;
+        j = 0;
         while j < alphaSize {
             if *length.offset(j as isize) as libc::c_int == i {
                 *perm.offset(pp as isize) = j;
@@ -209,43 +196,40 @@ pub unsafe fn BZ2_hbCreateDecodeTables(
         }
         i += 1;
     }
-    i = 0 as libc::c_int;
-    while i < 23 as libc::c_int {
-        *base.offset(i as isize) = 0 as libc::c_int;
+    i = 0;
+    while i < BZ_MAX_CODE_LEN as libc::c_int {
+        *base.offset(i as isize) = 0;
         i += 1;
     }
-    i = 0 as libc::c_int;
+    i = 0;
     while i < alphaSize {
-        let fresh0 = &mut (*base
-            .offset((*length.offset(i as isize) as libc::c_int + 1 as libc::c_int) as isize));
+        let fresh0 = &mut (*base.offset((*length.offset(i as isize) + 1) as isize));
         *fresh0 += 1;
         i += 1;
     }
-    i = 1 as libc::c_int;
-    while i < 23 as libc::c_int {
+    i = 1;
+    while i < BZ_MAX_CODE_LEN as libc::c_int {
         let fresh1 = &mut (*base.offset(i as isize));
-        *fresh1 += *base.offset((i - 1 as libc::c_int) as isize);
+        *fresh1 += *base.offset((i - 1) as isize);
         i += 1;
     }
-    i = 0 as libc::c_int;
-    while i < 23 as libc::c_int {
-        *limit.offset(i as isize) = 0 as libc::c_int;
+    i = 0;
+    while i < BZ_MAX_CODE_LEN as libc::c_int {
+        *limit.offset(i as isize) = 0;
         i += 1;
     }
-    vec = 0 as libc::c_int;
+    vec = 0;
     i = minLen;
     while i <= maxLen {
-        vec += *base.offset((i + 1 as libc::c_int) as isize) - *base.offset(i as isize);
-        *limit.offset(i as isize) = vec - 1 as libc::c_int;
-        vec <<= 1 as libc::c_int;
+        vec += *base.offset((i + 1) as isize) - *base.offset(i as isize);
+        *limit.offset(i as isize) = vec - 1;
+        vec <<= 1;
         i += 1;
     }
-    i = minLen + 1 as libc::c_int;
+    i = minLen + 1;
     while i <= maxLen {
-        *base.offset(i as isize) = ((*limit.offset((i - 1 as libc::c_int) as isize)
-            + 1 as libc::c_int)
-            << 1 as libc::c_int)
-            - *base.offset(i as isize);
+        *base.offset(i as isize) =
+            ((*limit.offset((i - 1) as isize) + 1) << 1) - *base.offset(i as isize);
         i += 1;
     }
 }
