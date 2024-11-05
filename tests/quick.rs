75165d04fbfa82e3800f87e366a770e0c1066bb5
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-11-05 16:52:25 +0100

    Fix testing for s390x

--- tests/quick.rs
+++ tests/quick.rs
@@ -1,14 +1,31 @@
+use std::env;
+use std::process::{Command, Stdio};
+
 fn run_test(compressed: &str, expected: &[u8]) {
-    let output = std::process::Command::new(env!("CARGO_BIN_EXE_bzip2"))
+    let mut cmd;
+    match env::var("RUNNER") {
+        Ok(runner) if !runner.is_empty() => {
+            let mut runner_args = runner.split(' ');
+            cmd = Command::new(runner_args.next().unwrap());
+            cmd.args(runner_args);
+            cmd.arg(env!("CARGO_BIN_EXE_bzip2"));
+        }
+        _ => cmd = Command::new(env!("CARGO_BIN_EXE_bzip2")),
+    }
+    let output = match cmd
         .arg("-d")
         .arg(compressed)
         .arg("-c")
-        .stdout(std::process::Stdio::piped())
+        .stdout(Stdio::piped())
         .output()
-        .unwrap();
+    {
+        Ok(output) => output,
+        Err(err) => panic!("Running {cmd:?} failed with {err:?}"),
+    };
     assert!(
         output.status.success(),
-        "{:?}",
+        "status: {:?} stderr: {:?}",
+        output.status,
         String::from_utf8_lossy(&output.stderr)
     );
     assert_eq!(output.stdout, expected);
