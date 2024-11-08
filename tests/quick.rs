3df18f86dcba362f8fb5e5bce8eb8a33524ac12a
Folkert de Vries <folkert@folkertdev.nl>
2024-11-08 14:05:03 +0100

    `bzip2.rs`: cleanup short flag parsing

--- tests/quick.rs
+++ tests/quick.rs
@@ -483,7 +483,11 @@ fn redundant_flag() {
         cmd.arg("--repetitive-best");
         let output = cmd.output().unwrap();
 
-        assert!(output.status.success());
+        assert!(
+            output.status.success(),
+            "{}",
+            String::from_utf8_lossy(&output.stderr)
+        );
 
         assert_eq!(
             String::from_utf8_lossy(&output.stderr).replace(bzip2_binary(), "bzip2"),
@@ -502,5 +506,26 @@ fn redundant_flag() {
             String::from_utf8_lossy(&output.stderr).replace(bzip2_binary(), "bzip2"),
             "bzip2: --repetitive-fast is redundant in versions 0.9.5 and above\n"
         );
+    }
+}
+
+#[test]
+fn bad_flag() {
+    {
+        let mut cmd = command();
+        cmd.arg("--foobar");
+        let output = cmd.output().unwrap();
+
+        assert!(!output.status.success(),);
+        assert!(String::from_utf8_lossy(&output.stderr).contains("Bad flag `--foobar'"));
+    }
+
+    {
+        let mut cmd = command();
+        cmd.arg("-x");
+        let output = cmd.output().unwrap();
+
+        assert!(!output.status.success());
+        assert!(String::from_utf8_lossy(&output.stderr).contains("Bad flag `-x'"));
     }
 }
