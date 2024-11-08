76da3c806f51172846bd18889f396cb53679b53f
Folkert de Vries <folkert@folkertdev.nl>
2024-11-08 14:11:41 +0100

    `assert_h`: use the `write!` macro

--- c2rust-lib.rs
+++ c2rust-lib.rs
@@ -85,7 +85,8 @@ pub(crate) struct AssertFail(i32);
 
 impl core::fmt::Display for AssertFail {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!(
+        write!(
+            f,
             concat!(
                 "\n",
                 "\n",
@@ -102,35 +103,38 @@ impl core::fmt::Display for AssertFail {
             ),
             self.0,
             libbzip2_rs_sys_version!(),
-        ))?;
+        )?;
 
         if self.0 == 1007 {
-            f.write_fmt(format_args!(concat!(
-                "\n",
-                "*** A special note about internal error number 1007 ***\n",
-                "\n",
-                "Experience suggests that a common cause of i.e. 1007\n",
-                "is unreliable memory or other hardware.  The 1007 assertion\n",
-                "just happens to cross-check the results of huge numbers of\n",
-                "memory reads/writes, and so acts (unintendedly) as a stress\n",
-                "test of your memory system.\n",
-                "\n",
-                "I suggest the following: try compressing the file again,\n",
-                "possibly monitoring progress in detail with the -vv flag.\n",
-                "\n",
-                "* If the error cannot be reproduced, and/or happens at different\n",
-                "  points in compression, you may have a flaky memory system.\n",
-                "  Try a memory-test program.  I have used Memtest86\n",
-                "  (www.memtest86.com).  At the time of writing it is free (GPLd).\n",
-                "  Memtest86 tests memory much more thorougly than your BIOSs\n",
-                "  power-on test, and may find failures that the BIOS doesn't.\n",
-                "\n",
-                "* If the error can be repeatably reproduced, this is a bug in\n",
-                "  bzip2, and I would very much like to hear about it.  Please\n",
-                "  let me know, and, ideally, save a copy of the file causing the\n",
-                "  problem -- without which I will be unable to investigate it.\n",
-                "\n"
-            )))?;
+            write!(
+                f,
+                concat!(
+                    "\n",
+                    "*** A special note about internal error number 1007 ***\n",
+                    "\n",
+                    "Experience suggests that a common cause of i.e. 1007\n",
+                    "is unreliable memory or other hardware.  The 1007 assertion\n",
+                    "just happens to cross-check the results of huge numbers of\n",
+                    "memory reads/writes, and so acts (unintendedly) as a stress\n",
+                    "test of your memory system.\n",
+                    "\n",
+                    "I suggest the following: try compressing the file again,\n",
+                    "possibly monitoring progress in detail with the -vv flag.\n",
+                    "\n",
+                    "* If the error cannot be reproduced, and/or happens at different\n",
+                    "  points in compression, you may have a flaky memory system.\n",
+                    "  Try a memory-test program.  I have used Memtest86\n",
+                    "  (www.memtest86.com).  At the time of writing it is free (GPLd).\n",
+                    "  Memtest86 tests memory much more thorougly than your BIOSs\n",
+                    "  power-on test, and may find failures that the BIOS doesn't.\n",
+                    "\n",
+                    "* If the error can be repeatably reproduced, this is a bug in\n",
+                    "  bzip2, and I would very much like to hear about it.  Please\n",
+                    "  let me know, and, ideally, save a copy of the file causing the\n",
+                    "  problem -- without which I will be unable to investigate it.\n",
+                    "\n"
+                )
+            )?;
         }
 
         Ok(())
