13b82f289c3dbd2924921922ed0ed3b4a68b9a3f
Folkert de Vries <folkert@folkertdev.nl>
2024-11-11 19:59:42 +0100

    `bzip2.rs`: move string into constant so it does not count as uncovered

--- bzip2.rs
+++ bzip2.rs
@@ -856,13 +856,14 @@ unsafe fn outOfMemory() -> ! {
 }
 
 unsafe fn configError() -> ! {
-    eprint!(concat!(
+    const MSG: &str = concat!(
         "bzip2: I'm not configured correctly for this platform!\n",
         "\tI require Int32, Int16 and Char to have sizes\n",
         "\tof 4, 2 and 1 bytes to run properly, and they don't.\n",
         "\tProbably you can fix this by defining them correctly,\n",
         "\tand recompiling.  Bye!\n",
-    ));
+    );
+    eprint!("{}", MSG);
     setExit(3 as libc::c_int);
     exit(exitValue);
 }
