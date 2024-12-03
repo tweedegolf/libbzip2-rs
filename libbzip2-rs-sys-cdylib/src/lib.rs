a94677fe41666b88cff74e1acdcc4e7daf4fdcab
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-12-03 11:57:37 +0100

    Make libbz2-rs-sys-cdylib unconditionally no_std
    This reduces the size of libbz2_rs.so by 408KB in release mode.

--- libbzip2-rs-sys-cdylib/src/lib.rs
+++ libbzip2-rs-sys-cdylib/src/lib.rs
@@ -1,3 +1,17 @@
+#![no_std]
+
 extern crate libbzip2_rs_sys;
 
+use core::ffi::c_int;
+use core::panic::PanicInfo;
 pub use libbzip2_rs_sys::*;
+
+#[panic_handler]
+fn panic_handler(_info: &PanicInfo) -> ! {
+    extern "C" {
+        fn bz_internal_error(errcode: c_int);
+    }
+
+    unsafe { bz_internal_error(-1) }
+    loop {}
+}
