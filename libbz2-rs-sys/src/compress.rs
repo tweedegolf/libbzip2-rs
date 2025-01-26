09e43750d96c21fe523e9e5889ca1458c492f284
Folkert de Vries <folkert@folkertdev.nl>
2025-01-26 14:49:24 +0100

    remove more manual unrolling

--- libbz2-rs-sys/src/compress.rs
+++ libbz2-rs-sys/src/compress.rs
@@ -560,34 +560,18 @@ fn send_mtf_values(s: &mut EState) {
         assert_h!((s.selector[selCtr] as usize) < nGroups, 3006);
         if nGroups == 6 && 50 == ge - gs + 1 {
             /*--- fast track the common case ---*/
-            let mut mtfv_i: u16;
 
             let s_len_sel_selCtr = s.len[s.selector[selCtr] as usize];
             let s_code_sel_selCtr = s.code[s.selector[selCtr] as usize];
 
-            macro_rules! BZ_ITAH {
-                ($nn:expr) => {
-                    mtfv_i = mtfv[(gs + $nn) as usize];
+            for chunk in mtfv[gs as usize..][..50].chunks_exact(10) {
+                for &mtfv_i in chunk {
                     writer.write(
-                        s_len_sel_selCtr[mtfv_i as usize] as i32,
-                        s_code_sel_selCtr[mtfv_i as usize],
+                        s_len_sel_selCtr[usize::from(mtfv_i)] as i32,
+                        s_code_sel_selCtr[usize::from(mtfv_i)],
                     );
-                };
+                }
             }
-
-            #[rustfmt::skip]
-            {
-                BZ_ITAH!(0);  BZ_ITAH!(1);  BZ_ITAH!(2);  BZ_ITAH!(3);  BZ_ITAH!(4);
-                BZ_ITAH!(5);  BZ_ITAH!(6);  BZ_ITAH!(7);  BZ_ITAH!(8);  BZ_ITAH!(9);
-                BZ_ITAH!(10); BZ_ITAH!(11); BZ_ITAH!(12); BZ_ITAH!(13); BZ_ITAH!(14);
-                BZ_ITAH!(15); BZ_ITAH!(16); BZ_ITAH!(17); BZ_ITAH!(18); BZ_ITAH!(19);
-                BZ_ITAH!(20); BZ_ITAH!(21); BZ_ITAH!(22); BZ_ITAH!(23); BZ_ITAH!(24);
-                BZ_ITAH!(25); BZ_ITAH!(26); BZ_ITAH!(27); BZ_ITAH!(28); BZ_ITAH!(29);
-                BZ_ITAH!(30); BZ_ITAH!(31); BZ_ITAH!(32); BZ_ITAH!(33); BZ_ITAH!(34);
-                BZ_ITAH!(35); BZ_ITAH!(36); BZ_ITAH!(37); BZ_ITAH!(38); BZ_ITAH!(39);
-                BZ_ITAH!(40); BZ_ITAH!(41); BZ_ITAH!(42); BZ_ITAH!(43); BZ_ITAH!(44);
-                BZ_ITAH!(45); BZ_ITAH!(46); BZ_ITAH!(47); BZ_ITAH!(48); BZ_ITAH!(49);
-            };
         } else {
             /*--- slow version which correctly handles all situations ---*/
             for i in gs..=ge {
